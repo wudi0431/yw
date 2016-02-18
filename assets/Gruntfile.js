e29878a2f2c8d205b6b0ae6718b4ab16db8d59f7
wudi <wudi2852987@163.com>
2016-02-18 15:13:10 +0800

    update index

--- assets/Gruntfile.js
+++ assets/Gruntfile.js
@@ -338,7 +338,7 @@ module.exports = function (grunt) {
         },
         concat: {
             dist: {
-                src: ['<%= config.app %>/scripts/*.js'],
+                src: ['<%= config.app %>/scripts/{,*/}.js'],
                 dest: '<%= config.dist %>/scripts/all.js',
                 filter: function (filepath) {
                     if(filepath.indexOf('main')==-1){
